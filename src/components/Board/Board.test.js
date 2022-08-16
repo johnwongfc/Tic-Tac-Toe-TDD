6a61bd63bd2549f2b932fb488024ba0323dc7932
johnwongfc <wongjohn377@gmail.com>
2022-08-16 14:36:27 -0700

    test-Board renders 9 Squares

--- src/components/Board/Board.test.js
+++ src/components/Board/Board.test.js
@@ -1,10 +1,32 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import Board from "./Board";
+import Square from "../Square/Square";
 
 describe("Board Testing", () => {
-  it("renders Board", () => {
-    const wrapper = shallow(<Board />);
-    expect(wrapper).toHaveLength(1);
+  let component;
+  let onClick;
+  let squares;
+
+  beforeEach(() => {
+    onClick = jest.fn();
+    squares = Array(9).fill(null);
+    component = mount(<Board squares={squares} onClick={onClick} />);
   });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("renders Board", () => {
+    // const wrapper = shallow(<Board />);
+    expect(component).toHaveLength(1);
+  });
+
+  it("squares are class square", () => {
+    expect(component.containsAllMatchingElements([<Square />])).toEqual(true);
+  });
+  it("renders 9 squares", () => {
+    expect(component.props().squares).toHaveLength(9);
+  })
 });
