f23ac6b135c30f6f7cb4d6c6e572d6d823a3567c
johnwongfc <wongjohn377@gmail.com>
2022-08-16 07:13:52 -0700

    square is clickable

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Square.css";
 
-const Square = ({ value }) => {
-  return <button className="Square">{value}</button>;
+const Square = ({ value, onClick }) => {
+  return <button className="Square" onClick={onClick}>{value}</button>;
 };
 
 export default Square;
