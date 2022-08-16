30453acd466436a13049796333b0a845c8c9992d
johnwongfc <wongjohn377@gmail.com>
2022-08-16 14:59:01 -0700

    test-Game renders Board

--- src/components/Game/Game.test.js
+++ src/components/Game/Game.test.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import Game from "./Game";
+import Board from "../Board/Board";
 
 describe("Game Testing", () => {
   it("renders Game", () => {
     const wrapper = shallow(<Game />);
     expect(wrapper).toHaveLength(1);
+  });
+
+  it("renders Board", () => {
+    const wrapper = mount(<Game />);
+    expect(wrapper.containsMatchingElement(<Board />)).toEqual(true);
   });
 });
