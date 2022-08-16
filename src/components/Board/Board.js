eacdde8bfe10a8cb7e8448b04727425f0ef293d6
johnwongfc <wongjohn377@gmail.com>
2022-08-16 14:36:36 -0700

    Board renders 9 Squares

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Board.css";
+import Square from "../Square/Square";
 
-const Board = () => {
-  return <div className="Board"></div>;
+const Board = ({ squares, onClick }) => {
+  return (
+    <div className="Board">
+      {squares.map((square, i) => (
+        <Square key={i} value={square} onClick={() => onClick(i)} />
+      ))}
+    </div>
+  );
 };
 
 export default Board;
