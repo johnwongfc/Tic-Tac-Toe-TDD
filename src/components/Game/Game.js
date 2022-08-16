d2a0aace760b52ca79e9af5688c555dd5e101466
johnwongfc <wongjohn377@gmail.com>
2022-08-16 15:08:23 -0700

    added state in funtional react hooks form

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Game.css";
 import Board from "../Board/Board";
 
 const Game = () => {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXisNext] = useState(true);
+  const xO = xIsNext ? "X" : "O";
+
   return (
   <div>
     <Board />
