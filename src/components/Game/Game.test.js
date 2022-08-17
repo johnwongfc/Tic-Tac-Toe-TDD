934129a95264b123c8f073a16821957cd059d082
johnwongfc <wongjohn377@gmail.com>
2022-08-17 10:55:41 -0700

    Test-Game shows draw and refactored game tests

--- src/components/Game/Game.test.js
+++ src/components/Game/Game.test.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { shallow, mount } from "enzyme";
 import Game from "./Game";
 import Board from "../Board/Board";
@@ -9,33 +9,52 @@ describe("Game Testing", () => {
     expect(wrapper).toHaveLength(1);
   });
 
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<Game />);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it("renders Board", () => {
-    const wrapper = mount(<Game />);
+    // const wrapper = mount(<Game />);
     expect(wrapper.containsMatchingElement(<Board />)).toEqual(true);
   });
 
   it("renders player's turn", () => {
-    const wrapper = mount(<Game />);
+    // const wrapper = mount(<Game />);
     const xO = "X";
     expect(wrapper.find(".player-display").text()).toBe(`Next player: ${xO}`);
   });
 
   it("renders X and O when clicked", () => {
-    const wrapper = mount(<Game />);
-    wrapper.find(".Square").first().simulate("click");
-    expect(wrapper.find(".Square").first().text()).toBe("X");
-    wrapper.find(".Square").last().simulate("click");
-    expect(wrapper.find(".Square").last().text()).toBe("O");
+    // const wrapper = mount(<Game />);
+    wrapper.find(".squares").first().simulate("click");
+    expect(wrapper.find(".squares").first().text()).toBe("X");
+    wrapper.find(".squares").last().simulate("click");
+    expect(wrapper.find(".squares").last().text()).toBe("O");
   });
 
   it("shows X wins", () => {
-    const wrapper = mount(<Game />);
     const steps = [0, 1, 3, 4, 6];
     const winner = "X";
 
     steps.forEach((step) => {
-      wrapper.find(".Square").at(step).simulate("click");
+      wrapper.find(".squares").at(step).simulate("click");
     });
     expect(wrapper.find(".player-display").text()).toBe(`Winner: ${winner}`);
+  });
+
+  it("should tell you if its a draw", () => {
+    const step = [0, 1, 2, 6, 7, 8, 3, 4, 5];
+
+    step.forEach((index) => {
+      wrapper.find(".squares").at(index).simulate("click");
+    });
+
+    expect(wrapper.find(".player-display").text()).toBe("It's a Draw!");
   });
 });
