30e8dbc694121bab32a0fe221625a2e80a0b201d
johnwongfc <wongjohn377@gmail.com>
2022-08-16 22:42:54 -0700

    Game renders X when clicked once

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -8,9 +8,21 @@ const Game = () => {
   const [xIsNext, setXisNext] = useState(true);
   const xO = xIsNext ? "X" : "O";
 
+  const handleClick = (i) => {
+    const historyPoint = history.slice(0, stepNumber + 1);
+    const current = historyPoint[stepNumber];
+    const squares = [...current];
+
+    // click square
+    squares[i] = xO;
+    setHistory([...historyPoint, squares]);
+    setStepNumber(historyPoint.length);
+    setXisNext(!xIsNext);
+  };
+
   return (
     <div>
-      <Board />
+      <Board squares={history[stepNumber]} onClick={handleClick} />
       <div className="player-display">{`Next player: ${xO}`}</div>
     </div>
   );
