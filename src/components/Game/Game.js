33b51a1320df7f90ff39a26aa86319e87eaa2719
johnwongfc <wongjohn377@gmail.com>
2022-08-17 00:28:03 -0700

    Shows X wins with winner logic

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import "./Game.css";
 import Board from "../Board/Board";
+import { calculateWinner } from "../../utils/helper";
 
 const Game = () => {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXisNext] = useState(true);
   const xO = xIsNext ? "X" : "O";
+  const winner = calculateWinner(history[stepNumber]);
 
   const handleClick = (i) => {
     const historyPoint = history.slice(0, stepNumber + 1);
     const current = historyPoint[stepNumber];
     const squares = [...current];
+
+    if (winner || squares[i]) return;
 
     // click square
     squares[i] = xO;
@@ -23,7 +27,10 @@ const Game = () => {
   return (
     <div>
       <Board squares={history[stepNumber]} onClick={handleClick} />
-      <div className="player-display">{`Next player: ${xO}`}</div>
+      {/* <div className="player-display">{`Next player: ${xO}`}</div> */}
+      <h3 className="player-display">
+        {winner ? `Winner: ${winner}` : `Next player: ${xO}`}
+      </h3>
     </div>
   );
 };
