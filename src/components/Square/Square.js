6c18c10bbdf14a264d31359f74319b1d2db14484
johnwongfc <wongjohn377@gmail.com>
2022-08-17 13:09:21 -0700

    Square changes styles with different input

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./Square.css";
 
 const Square = ({ value, onClick }) => {
-  return <button className="squares" onClick={onClick}>{value}</button>;
+  const style = value ? `squares ${value}` : "squares";
+  return (
+    <button className={style} onClick={onClick}>
+      {value}
+    </button>
+  );
 };
 
 export default Square;
